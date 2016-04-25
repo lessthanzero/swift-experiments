466f565f50629def45ad536d8a608e19ceeaf42a
Alexander Katin <aleksandr.katin@gmail.com>
2016-04-25 15:55:05 +0300

    start new workspace, refresh basic concepts

--- 1-playground/swiftPlayground.playground/Contents.swift
+++ 1-playground/swiftPlayground.playground/Contents.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let swiftTeam = 13
+let iOSTeam = 54
+let otherTeams = 48
+var totalTeam = swiftTeam + iOSTeam + otherTeams
+
+
+// Basic types and control flow
+
+let priceInferred = 19.99
+let priceExcplicit: Double = 19.99
+
+let onSaleInferred = true
+let onSaleExcplicit: Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExcplicit: String = "Whoopie Cushion"
+
+if onSaleInferred {
+    print("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    print("\(nameInferred) at regular price: \(priceInferred)!")
+}
